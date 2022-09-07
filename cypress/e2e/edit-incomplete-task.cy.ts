a79dcceb46dfd65d4ee11fdd3a20205d338f4e2e
Axel Isouard <axel@isouard.fr>
2022-09-07 19:46:52 +0200

    test(e2e): create the task instead of retrieving it

--- cypress/e2e/edit-incomplete-task.cy.ts
+++ cypress/e2e/edit-incomplete-task.cy.ts
@@ -2,6 +2,12 @@ describe('Edit incomplete task', () => {
   it('edits an incomplete task', () => {
     cy.visit('/');
 
+    cy.get('[data-qa="new-task-name"]')
+      .type('It will never be done');
+
+    cy.get('[data-qa="create-task-button"]')
+      .click();
+
     cy.get('[data-qa="incomplete-tasks"]')
       .contains('It will never be done');
 
